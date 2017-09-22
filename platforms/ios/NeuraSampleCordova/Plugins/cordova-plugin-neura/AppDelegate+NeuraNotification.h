aace0b1004cd4472bff9c2f4c35ac8e07438ec15
jeweller1980stepanets <jeweller1980@gmail.com>
2017-09-22 13:18:55 +0300

    deleted platforms

--- platforms/ios/NeuraSampleCordova/Plugins/cordova-plugin-neura/AppDelegate+NeuraNotification.h
+++
@@ -1,4 +0,0 @@
-#import "AppDelegate.h"
-
-@interface AppDelegate (NeuraNotification)
-@end
--- platforms/ios/Pods/Target Support Files/Pods-NeuraSampleCordova/Pods-NeuraSampleCordova-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_NeuraSampleCordova : NSObject
-@end
-@implementation PodsDummy_Pods_NeuraSampleCordova
-@end
